8bda678b52534968f0cc1b136495449619acbe18
navegabit <navegabit.2020@gmail.com>
2024-04-24 01:00:24 +0200

    Feature: Improve responsive design

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
